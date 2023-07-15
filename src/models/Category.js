856e9638e64d9f18b2684a23a98eea4177599bf3
Gabriel <scapimgabriel@gmail.com>
2023-07-15 08:55:21 -0300

    fix: category model

--- src/models/Category.js
+++ src/models/Category.js
@@ -1,6 +1,10 @@
 const CategorySchema = (sequelize, DataTypes) => {
     const Category = sequelize.define('Category', {
-        id: DataTypes.INTEGER,
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+        },
         name: DataTypes.STRING,
     }, 
     {
